61d4e1d2469875aabe1f0efd237843ffffbb29c1
Esra Biyik <esra.biyik@idesse.com.tr>
2022-08-22 10:10:15 +0300

    bug fix

--- Homework_9/Pages/Index.cshtml.cs
+++ Homework_9/Pages/Index.cshtml.cs
@@ -6,26 +6,63 @@ namespace Homework_9.Pages {
   public class IndexModel : PageModel {
     private readonly ILogger<IndexModel> _logger;
 
+
+
+    [BindProperty(SupportsGet = true)]
+    public string UserName { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public string Password { get; set; }
+
     public IndexModel(ILogger<IndexModel> logger) {
       _logger = logger;
     }
 
 
-    [BindProperty]
-    public string ErrorMessage { get; set; } = String.Empty;
 
-    [BindProperty]
-    public string UserName { get; set; } = String.Empty;
-    [Required]
-    [BindProperty]    
-    public string Password { get; set; } = String.Empty;
+
     public void OnGet() {
 
 
     }
 
+    //public PartialViewResult OnPostLogin(string username, string password) {
+
+
+    //  if (String.IsNullOrEmpty(password) == true) {
+
+    //    throw new Exception("Password can not be null or empty");
+    //    //ErrorMessage = "Password can not be null or empty";
+    //    //return Partial("_UserError", this);
+    //  } else
+
+    //    throw new Exception("Password can not be null or empty");
+
+    //  return Partial("Types");
+    //}
+
+
+    public IActionResult OnPostLogin(string username, string password) {
+      
+      
+      
+      if (String.IsNullOrEmpty(password)) {
+
+        return RedirectToPage("Index");
+      }
+      else
+      return RedirectToPage("Main");
+    }
+
+
+
+
 
 
   }
+
+ 
+
+
 
 }
---
+++ Homework_9/Pages/Main.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Homework_9.Pages
+{
+    public class MainModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
